eaa944a20350e6b77577a7c1f5951a38f46601a8
Jonathan Björnebro <jonathan.bjornebro@outlook.com>
2021-05-11 17:26:39 +0200

    Long overdue

--- Core/Services/BankApiClient.cs
+++ Core/Services/BankApiClient.cs
@@ -29,7 +29,7 @@ namespace Core.Services
         }
         public async Task<List<Account>> GetAccounts(int customerId)
         {
-            return await client.GetFromJsonAsync<List<Account>>($"api/customer/GetAccounts/{customerId}");
+            return await client.GetFromJsonAsync<List<Account>>($"api/customer/Accounts/{customerId}");
         }
         public async Task<List<Transaction>> GetTransactionsAsync(int accountId)
         {
@@ -51,16 +51,36 @@ namespace Core.Services
         {
 
         }
-        public async Task<List<Customer>> GetCustomerPageAsync(int page)
+        public async Task<List<Customer>> GetCustomerPageAsync(string searchString, int page)
         {
-            int offset = page * 10;
-
-            if (page == 0)
+            if(searchString.StartsWith('#'))
             {
-                offset = 0;
+                searchString = searchString.Remove(0, 1);
+                return await client.GetFromJsonAsync<List<Customer>>($"api/Customer/{searchString}");
             }
+            else
+            {
+                int offset = page * 10;
 
-            return await client.GetFromJsonAsync<List<Customer>>($"api/Customer/?offset={offset}");
+                if (page == 0)
+                {
+                    offset = 0;
+                }
+
+                return await client.GetFromJsonAsync<List<Customer>>($"api/Customer/?searchString={searchString}&offset={offset}");
+            }
+        }
+        public async Task<Customer> GetCustomer(int id)
+        {
+            return await client.GetFromJsonAsync<Customer>($"api/Customer/{id}");
+        }
+        public async Task<Customer> GetAccount(int id)
+        {
+            return await client.GetFromJsonAsync<Customer>($"api/Account/{id}");
+        }
+        public async Task<List<AccountType>> GetAccountTypes()
+        {
+            return await client.GetFromJsonAsync<List<AccountType>>($"api/AccountType/all");
         }
 
     }
